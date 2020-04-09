583338fc6d754f5d1240b2411bc22f5b583ee40b
Aqdas <aqdasak@gmail.com>
2020-04-09 13:30:32 +0530

    initial commit

---
+++ Prettify Folder.py
@@ -0,0 +1,18 @@
+import os
+
+
+def soldier(path, exceptFile, ext):
+    os.chdir(path)
+    files = os.listdir(path)
+    i = 1
+    with open(exceptFile) as f:
+        exceptFileList = f.read().split("\n")
+    for file in files:
+        if file not in exceptFileList:
+            os.rename(file, file.capitalize())
+            if os.path.splitext(file)[1] == ext:
+                os.rename(file, f"{i}{ext}")
+                i += 1
+
+
+soldier("D:\snak","a.txt", ".ico")
