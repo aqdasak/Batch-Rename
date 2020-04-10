adb8012ae23d99e031ec7049d63c7ac86d231fd5
Aqdas <aqdasak@gmail.com>
2020-04-10 12:19:24 +0530

    added choice in batch rename or capitalize

--- Prettify Folder.py
+++ Prettify Folder.py
@@ -1,41 +1,42 @@
 import os
 
 
-def capital(path, avoid):
-    os.chdir(path)
-    files = os.listdir(path)
+def batch_rename(files, avoid, start):
+    i = start
+    for file in files:
+        if file not in avoid:
+            os.rename(file, f"{i}{os.path.splitext(file)[1]}")
+            i += 1
+
+
+def capital(files, avoid):
     for file in files:
         if file not in avoid:
             os.rename(file, file.capitalize())
 
-
-def batch_rename(path, avoid):
-    os.chdir(path)
-    files = os.listdir(path)
-    i = 1
-    for file in files:
-        if file not in avoid:
-            os.rename(file, f"i{os.path.splitext(file)[1]}")
-            i += 1
-
-
-# soldier("D:\zsnak", "a.txt", ".ico")
 
 if __name__ == '__main__':
     print("1. Batch Rename\n2. Capitalize I letter")
     userChoice = input()
     print("Input the path of the folder that contains the files to be renamed")
     path = input()
-    a = input("Do you want add some files in the ignore list (y/n): ")
-    if a == "y":
-        exceptFileList = []
-        print("Enter ':q' to exit")
-        while True:
-            st = input()
-            if st == ':q':
-                break
-            exceptFileList.append(st)
+    print(
+        "If you want to ignore some files, add name of those files in pfavd.txt \nin the same folder in which files to be renamed are present.")
+    os.chdir(path)
+    files = os.listdir(path)
+    print(files)
+    avoid = []
+    try:
+        with open("pfavd.txt") as f:
+            avoid = f.read().split("\n")
+            # print("1>>>>>",avoid)
+    except:
+        print("No file ignored")
+    avoid.append("pfavd.txt")
+    # print("2>>>>>", avoid)
+
     if userChoice == "1":
-        batch_rename(path, exceptFileList)
+        start = int(input("Enter a number from which renaming counter will start : "))
+        batch_rename(files, avoid, start)
     elif userChoice == "2":
-        capital(path, exceptFileList)
+        capital(files, avoid)
