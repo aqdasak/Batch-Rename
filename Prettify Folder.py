efd9ae6543218e770cf543f23a8d8cfb981724e5
Aqdas <aqdasak@gmail.com>
2020-04-11 12:00:49 +0530

    rename after arranging accoording to extension

--- Prettify Folder.py
+++ Prettify Folder.py
@@ -1,42 +1,75 @@
 import os
+from time import sleep
 
 
 def batch_rename(files, avoid, start):
     i = start
     for file in files:
         if file not in avoid:
-            os.rename(file, f"{i}{os.path.splitext(file)[1]}")
+            try:
+                os.rename(file, f"{i}{os.path.splitext(file)[1]}")
+            except Exception as e:
+                print(e)
+            else:
+                print(f"{file} renamed")
             i += 1
 
 
 def capital(files, avoid):
     for file in files:
         if file not in avoid:
-            os.rename(file, file.capitalize())
+            try:
+                os.rename(file, file.capitalize())
+            except Exception as  e:
+                print(e)
+            else:
+                print(f"{file} capitalized")
+
+def arrange_by_extension(files):
+    # Using list comprehension to get a list of extionsions of all files and saving them into a set to avoid repetitive values
+    ext_set = set([os.path.splitext(file)[1] for file in files])
+    # Converting set into list as 'set' object is not subscriptable
+    ext_set = list(ext_set)
+
+    files2 = []
+    i = 0
+    while i < len(ext_set):
+        for file in files:
+            if os.path.splitext(file)[1] == ext_set[i]:
+                files2.append(file)
+        i += 1
+    return files2
 
 
 if __name__ == '__main__':
     print("1. Batch Rename\n2. Capitalize I letter")
     userChoice = input()
+    if userChoice not in ["1","2"]:
+        print("Wrong input")
+        exit()
+    print(
+        "# If you want to ignore some files, add name of those files in pfavd.txt \n# in the same folder in which files to be renamed are present.\n")
     print("Input the path of the folder that contains the files to be renamed")
     path = input()
-    print(
-        "If you want to ignore some files, add name of those files in pfavd.txt \nin the same folder in which files to be renamed are present.")
+
     os.chdir(path)
-    files = os.listdir(path)
-    print(files)
+    files = arrange_by_extension(os.listdir(path))
+
     avoid = []
     try:
         with open("pfavd.txt") as f:
             avoid = f.read().split("\n")
-            # print("1>>>>>",avoid)
     except:
         print("No file ignored")
     avoid.append("pfavd.txt")
-    # print("2>>>>>", avoid)
 
     if userChoice == "1":
         start = int(input("Enter a number from which renaming counter will start : "))
-        batch_rename(files, avoid, start)
+        cnfrm = input("This can't be undone. Are you sure(y/n) : ").lower()
+        if cnfrm == "y" or cnfrm == "yes":
+            batch_rename(files, avoid, start)
+        else:
+            print("Nothing renamed")
     elif userChoice == "2":
         capital(files, avoid)
+sleep(2)
